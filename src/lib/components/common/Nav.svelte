ad028114f3c157c7ce8e80a323e75649f2b49526
BlasmethiaN <dominako223@gmail.com>
2022-07-08 13:34:02 +0200

    ✨ Added windicss

---
+++ src/lib/components/common/Nav.svelte
@@ -0,0 +1,7 @@
+<script>
+	import 'virtual:windi.css'
+</script>
+
+<nav>
+	<div class="flex flex-row text-xl text-yellow-500">nav neco</div>
+</nav>
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,8 +8,12 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$src: './src',
+			$lib: './src/lib'
+		}
 	}
-};
+}
 
-export default config;
+export default config
